2fdc0e8273c0f0f1984e9af858c9c044c92ee362
dane-king <kingd9@gmail.com>
2017-05-21 06:03:44 -0400

    adding webpack and babel

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
+1. Create a new repo at github.
+2. Clone the repo from fedorahosted to your local machine.
+3. git remote add origin URL_TO_GITHUB_REPO
+4. git push origin master
+
+
 * Trying out AVA for tests
 `$ npm install --global ava
 $ ava --init`
 
 * run by
 `ava` or `ava --watch`
+
+
+* Webpack is set up for babel
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+
+};
